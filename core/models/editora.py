81227b350c105d75bfb4c0a971138ca1eafd7d7e
gabrielcmohr <gabrielmohfeg@gmail.com>
2025-05-30 22:42:22 -0300

    😭😭😭

--- core/models/editora.py
+++ core/models/editora.py
@@ -4,6 +4,7 @@ class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
